679bb74a6fa7fd08c1aed5d9862679a704bb0f2a
Seth Littleford <seth.littleford@gmail.com>
2021-08-14 14:21:46 -0600

    wrap megan logo in a link to home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import * as styles from './header.module.css'
 import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.content}>
-        <StaticImage 
-          src="../images/Megan_Littleford_Design_Logo.png" 
-          alt="Megan Littleford Design" 
-          placeholder="blurred"
-          layout="fixed"
-          height={62}
-        />
+        <Link to="/">
+          <StaticImage 
+            src="../images/Megan_Littleford_Design_Logo.png" 
+            alt="Megan Littleford Design" 
+            placeholder="blurred"
+            layout="fixed"
+            height={62}
+          />
+        </Link>
       </div>
     </header>
   )
