2ae116d5de27b47c11847ea9ce49b4847a78d3de
Seth Littleford <seth.littleford@gmail.com>
2021-08-14 14:04:25 -0600

    add megan logo to header

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
     title: "megan-design",
   },
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     // {
     //   resolve: "gatsby-plugin-google-analytics",
     //   options: {
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import * as styles from './header.module.css'
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.content}>
-        Image Gallery Collaborations About Me Contact
+        <StaticImage 
+          src="../images/Megan_Littleford_Design_Logo.png" 
+          alt="Megan Littleford Design" 
+          placeholder="blurred"
+          layout="fixed"
+          height={62}
+        />
       </div>
     </header>
   )
