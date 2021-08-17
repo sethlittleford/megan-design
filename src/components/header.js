979c1c86752379329161d3da190e885bcedf58e8
Seth Littleford <seth.littleford@gmail.com>
2021-08-16 22:18:39 -0600

    mobile menu animation slide in out on x axis

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as styles from './header.module.css'
 import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
 import Hamburger from '../svg/hamburger.svg';
 
 export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => setMobileMenuOpen(!mobileMenuOpen);
   return (
     <header className={styles.header}>
       <nav className={styles.content}>
@@ -17,13 +19,13 @@ export default function Header() {
             height={62}
           />
         </Link>
-        <div className={styles.mobileMenu}>
+        <div className={`${styles.mobileMenu} ${mobileMenuOpen ? styles.openMenu : ''}`}>
           <Link to="/" activeClassName={styles.activeLink}>Gallery</Link>
           <Link to="/" activeClassName={styles.activeLink}>Collaborations</Link>
           <Link to="/" activeClassName={styles.activeLink}>About Me</Link>
           <Link to="/" activeClassName={styles.activeLink}>Contact</Link>
         </div>
-        <button className={styles.hamburger}>
+        <button className={styles.hamburger} onClick={toggleMobileMenu}>
           <div className={styles.circle}>
             <Hamburger />
           </div>
