77fe2c8ab1b97adc20d6c31807db20e6028d84d6
Seth Littleford <seth.littleford@gmail.com>
2021-08-20 09:32:58 -0600

    remove the react plugin for svg in favor of svgr script

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /svg/
-        }
-      }
-    },
     // {
     //   resolve: "gatsby-plugin-google-analytics",
     //   options: {
