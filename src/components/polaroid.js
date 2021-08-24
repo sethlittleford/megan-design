968d82e51f3eab08449665409529493cdaba4f5a
Seth Littleford <seth.littleford@gmail.com>
2021-08-23 22:26:08 -0600

    source images from filesystem and use graphql StaticQuery for GatsbyImage

--- src/components/polaroid.js
+++ src/components/polaroid.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import * as styles from './polaroid.module.css'
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 
 const Polaroid = () => {
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     allImageSharp {
+  //       edges {
+  //         node {
+  //           gatsbyImageData(width: 293, height: 293, placeholder: BLURRED)
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: {eq: "Megan_Littleford_Design_Logo.png"}) {
+        childImageSharp {
+          gatsbyImageData(width: 293, height: 293, placeholder: BLURRED)
+        }
+      }
+    }
+  `)
+  console.log(data);
+  const image = getImage(data.file);
   return (
     <div className={styles.polaroid}>
-      Polaroid
+      <GatsbyImage image={image} alt="some alt description" />
     </div>
   );
 }
