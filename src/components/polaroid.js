0f615840239cb14512edf5df08125f6ab119fb20
Seth Littleford <seth.littleford@gmail.com>
2021-08-23 21:15:19 -0600

    polaroid container

---
+++ src/components/polaroid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as styles from './polaroid.module.css'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+
+const Polaroid = () => {
+  return (
+    <div className={styles.polaroid}>
+      Polaroid
+    </div>
+  );
+}
+
+export default Polaroid;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Polaroid from '../components/polaroid'
 
 const IndexPage = () => {
-  return <Layout><div>This is the home page Gallery.</div></Layout>
+  return (
+    <Layout>
+      <div>This is the home page Gallery.</div>
+      <Polaroid />
+    </Layout>
+  );
 }
 
 export default IndexPage
