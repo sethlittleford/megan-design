73a6c6e15b07dfbfd889f76702f421657fd95d14
Seth Littleford <seth.littleford@gmail.com>
2021-08-26 22:06:04 -0600

    page query for all images, map images to Polaroid component

--- src/components/polaroid.js
+++ src/components/polaroid.js
@@ -3,42 +3,22 @@ import * as styles from './polaroid.module.css'
 import { useStaticQuery, graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-const Polaroid = () => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     allImageSharp {
-  //       edges {
-  //         node {
-  //           gatsbyImageData(width: 293, height: 293, placeholder: BLURRED)
+const Polaroid = ({ image }) => {
+  // const { placeholderImage } = useStaticQuery(
+  //   graphql`
+  //     query {
+  //       placeholderImage: file(relativePath: { eq: "bluebirds.png" }) {
+  //         childImageSharp {
+  //           gatsbyImageData(
+  //             placeholder: BLURRED
+  //             formats: [AUTO, WEBP, AVIF]
+  //           )
   //         }
   //       }
   //     }
-  //   }
-  // `)
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     file(relativePath: {eq: "Megan_Littleford_Design_Logo.png"}) {
-  //       childImageSharp {
-  //         gatsbyImageData(width: 293, height: 293, placeholder: BLURRED)
-  //       }
-  //     }
-  //   }
-  // `)
-  const { placeholderImage } = useStaticQuery(
-    graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "bluebirds.png" }) {
-          childImageSharp {
-            gatsbyImageData(
-              placeholder: BLURRED
-              formats: [AUTO, WEBP, AVIF]
-            )
-          }
-        }
-      }
-    `
-  )
-  const image = getImage(placeholderImage);
+  //   `
+  // )
+  // const image = getImage(placeholderImage);
   return (
     <div className={styles.polaroid}>
       <GatsbyImage image={image} alt={"testimage"} className={styles.imageWrapper}/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Polaroid from '../components/polaroid'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <Layout>
       <div>This is the home page Gallery.</div>
-      <Polaroid />
+      {data.allImageSharp.edges.map(edge => {
+        let image = getImage(edge.node);
+        // return <GatsbyImage image={seth} alt={"testimage"}/> 
+        return <Polaroid image={image} />
+      })}
     </Layout>
   );
 }
+
+export const pageQuery = graphql`
+  query {
+    allImageSharp {
+      edges {
+        node {
+          gatsbyImageData(
+            placeholder: BLURRED
+            formats: [AUTO, WEBP, AVIF]
+          )
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
