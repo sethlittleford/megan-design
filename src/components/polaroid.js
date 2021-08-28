1e826517242129ccff95c46fda0a304501f7e252
Seth Littleford <seth.littleford@gmail.com>
2021-08-27 22:20:39 -0600

    cleanup + style polaroid component layout

--- src/components/polaroid.js
+++ src/components/polaroid.js
@@ -3,21 +3,6 @@ import * as styles from './polaroid.module.css'
 import { GatsbyImage } from 'gatsby-plugin-image'
 
 const Polaroid = ({ image, alt, children }) => {
-  // const { placeholderImage } = useStaticQuery(
-  //   graphql`
-  //     query {
-  //       placeholderImage: file(relativePath: { eq: "bluebirds.png" }) {
-  //         childImageSharp {
-  //           gatsbyImageData(
-  //             placeholder: BLURRED
-  //             formats: [AUTO, WEBP, AVIF]
-  //           )
-  //         }
-  //       }
-  //     }
-  //   `
-  // )
-  // const image = getImage(placeholderImage);
   return (
     <div className={styles.polaroid}>
       <GatsbyImage image={image} alt={alt} className={styles.imageWrapper} imgClassName={styles.image}/>
