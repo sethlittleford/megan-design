d81424fc0b2a83517019c19d33b9a21e9dd3269d
Seth Littleford <seth.littleford@gmail.com>
2021-08-29 13:07:22 -0600

    creating grid layout for polaroid in the gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,24 @@ import Polaroid from '../components/polaroid'
 import { graphql } from 'gatsby'
 import { getImage } from 'gatsby-plugin-image'
 import { mapImageToSVG } from '../components/handwritten-svgs/map-image-to-svg'
+import * as styles from './index.module.css'
 
 const IndexPage = ({ data }) => {
   return (
     <Layout>
       <div>This is the home page Gallery.</div>
-      {data.allFile.nodes.map(node => {
-        let image = getImage(node);
-        let alt = node.childImageSharp.parent.name;
-        let SVGComponent = mapImageToSVG.get(alt);
-        return <Polaroid image={image} alt={alt}>{SVGComponent}</Polaroid>
-      })}
+      <div className={styles.grid}>
+        {data.allFile.nodes.map(node => {
+          let image = getImage(node);
+          let alt = node.childImageSharp.parent.name;
+          let SVGComponent = mapImageToSVG.get(alt);
+          return <Polaroid image={image} alt={alt}>{SVGComponent}</Polaroid>
+        })}
+        <div className={styles.fakePolaroid}>polaroid</div>
+        <div className={styles.fakePolaroid}>polaroid</div>
+        <div className={styles.fakePolaroid}>polaroid</div>
+        <div className={styles.fakePolaroid}>polaroid</div>
+      </div>
     </Layout>
   );
 }
