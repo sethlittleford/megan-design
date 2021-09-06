05023c964fd16d0ad84b8c80ad32f7c8c9d0cfb9
Seth Littleford <seth.littleford@gmail.com>
2021-09-06 09:45:36 -0600

    <Link> wrapper around polaroid component

--- src/components/polaroid.js
+++ src/components/polaroid.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import * as styles from './polaroid.module.css'
 import { GatsbyImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 
 const Polaroid = ({ image, alt, children }) => {
   return (
-    <div className={styles.polaroid}>
-      <GatsbyImage image={image} alt={alt} className={styles.imageWrapper} imgClassName={styles.image}/>
-      {children}
-    </div>
+    <Link to={`${alt}`}>
+      <div className={styles.polaroid}>
+        <GatsbyImage image={image} alt={alt} className={styles.imageWrapper} imgClassName={styles.image}/>
+        {children}
+      </div>
+    </Link>
   );
 }
 
