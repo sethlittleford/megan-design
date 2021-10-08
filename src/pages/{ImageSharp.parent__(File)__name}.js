3eb1016fd18042f07b94e1f8e3ebea05a790569a
Seth Littleford <seth.littleford@gmail.com>
2021-10-08 13:55:51 -0600

    PolaroidDetail component displays pattern image

--- src/pages/{ImageSharp.parent__(File)__name}.js
+++ src/pages/{ImageSharp.parent__(File)__name}.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
 
-const PolaroidDetail = () => {
-    return "Implement Me"
+
+const PolaroidDetail = (props) => {
+  const imageName = props.params.parent__name
+  return <GatsbyImage image={props.data.imageSharp.gatsbyImageData} alt={imageName} />
 }
+
+export const query = graphql`
+  query($id: String) {
+    imageSharp(id: { eq: $id }) {
+        id
+        gatsbyImageData
+    }
+  }
+`
 
 export default PolaroidDetail
