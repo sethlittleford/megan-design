3874236f7a02e9c493ef5ca91081598ade634532
Seth Littleford <seth.littleford@gmail.com>
2021-10-08 15:48:32 -0600

    layout and styling for polaroid detail pages

--- src/pages/{ImageSharp.parent__(File)__name}.js
+++ src/pages/{ImageSharp.parent__(File)__name}.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { graphql } from 'gatsby'
-
+import Layout from '../components/layout'
+import * as styles from './PolaroidDetail.module.css'
+import { mapImageToSVG } from '../components/handwritten-svgs/map-image-to-svg'
 
 const PolaroidDetail = (props) => {
   const imageName = props.params.parent__name
-  return <GatsbyImage image={props.data.imageSharp.gatsbyImageData} alt={imageName} />
+  let SVGComponent = mapImageToSVG.get(props.data.imageSharp.parent.name)
+  return(
+    <Layout>
+      <div className={styles.container}>
+        <GatsbyImage image={props.data.imageSharp.gatsbyImageData} alt={imageName} className={styles.image}/>
+        <div className={styles.svgContainer}>{SVGComponent}</div>
+      </div>
+    </Layout>
+  );
 }
 
 export const query = graphql`
   query($id: String) {
     imageSharp(id: { eq: $id }) {
-        id
-        gatsbyImageData
+      id
+      gatsbyImageData
+      parent {
+        ... on File {
+          name
+        }
+      }
     }
   }
 `
